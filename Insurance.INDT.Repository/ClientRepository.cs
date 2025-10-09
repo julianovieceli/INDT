7780f1664b4e1b778a4903649589db284717be78
julianovieceli <125666080+julianovieceli@users.noreply.github.com>
2025-10-09 16:17:16 -0300

    Add files via upload

---
+++ Insurance.INDT.Repository/ClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using Insurance.INDT.Domain.Interfaces.Repository;
+
+namespace Insurance.INDT.Repository
+{
+    public class ClientRepository: IClientRepository
+    {
+        public Task<bool> GetByDocto(string docto)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Insurance.INDT.Repository/IoC.cs
@@ -0,0 +1,13 @@
+﻿using Insurance.INDT.Domain.Interfaces.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Insurance.INDT.Repository
+{
+    public static class Ioc
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services.AddScoped<IClientRepository, ClientRepository>();
+        }
+    }
+}
