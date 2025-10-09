fed6a600aded480f1c411519449ddb5c19cd2395
julianovieceli <125666080+julianovieceli@users.noreply.github.com>
2025-10-09 16:16:54 -0300

    Add files via upload

---
+++ Insurance.INDT.Dto/Request/RegisterProposalInsuranceDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Insurance.INDT.Dto.Request
+{
+    public class RegisterProposalInsuranceDto
+    {
+        public ResisterInsuranceDto Insurance { get; set; }
+
+        public RegisterClientDto Client { get; set; }
+    }
+
+    public class ResisterInsuranceDto
+    {
+        public string Name { get; set; }
+
+        public decimal Value { get; set; }
+    }
+
+    public class RegisterClientDto
+    {
+        public string Name { get; set; }
+
+        public string Docto { get; set; }
+        public int Age { get; set; }
+    }
+}
