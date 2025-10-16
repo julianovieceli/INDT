5fff9eda3661f78d200f040ea046fc1c9200d6ca
Juliano Vieceli <julianovieceli@gmail.com>
2025-10-16 17:09:20 -0300

    organizacao projeto mongo

---
+++ INDT.Common/INDT.Common.Insurance.Infra.MongoDb.Repository/MongoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using INDT.Common.Insurance.Domain.Interfaces.Repository.Mongo;
+using MongoDB.Driver;
+
+namespace INDT.Common.Insurance.Infra.MongoDb.Repository
+{
+    public class MongoDbContext: IMongoDbcontext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+    }
+}
